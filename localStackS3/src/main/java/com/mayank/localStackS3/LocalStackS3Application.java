02a84088889eda12b12a39c1dc3edbff2ca3f3fe
mayanknetam1234 <netammayank547@gmail.com>
2025-12-28 13:13:09 +0530

    first actual commit

---
+++ localStackS3/src/main/java/com/mayank/localStackS3/LocalStackS3Application.java
@@ -0,0 +1,13 @@
+package com.mayank.localStackS3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalStackS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalStackS3Application.class, args);
+	}
+
+}
---
+++ localStackS3/src/test/java/com/mayank/localStackS3/LocalStackS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.localStackS3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalStackS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
